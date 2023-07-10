eeaa68f7b8458f521a7e38d084ee083135796c48
Alessandra Maria <beh.090299@gmail.com>
2023-07-10 19:28:37 -0300

    :tada: initial commit

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light")
+  html.classList.toggle('light')
 
-  /* 1. condicional botão liga/desliga:
-  if (html.classList.contains('light')) { 
-    html.classList.remove('light') 
-  } else { 
-    html.classList.add('light') 
-  } */
+  // if (html.classList.contains('light')) {
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
 
-  // 2. trocar a imagem junto:
-  //pegar a tag img;
+
+  // pegar a tag img
   const img = document.querySelector("#profile img")
-  
-  //condição:
-  //substituir a imagem.
-  if (html.classList.contains("light")) {
-    //se tiver light mode, adicionar a imagem light;
-    img.setAttribute("src", "./assets/assets/avatar B 2.png")
-    
-  } else {
-    //senão, manter a imagem normal.
-    img.setAttribute("src", "./assets/assets/avatar B.png")
+
+  // substituir a imagem
+  if (html.classList.contains('light')) { 
+  // se tiver light mode, adicionar a imagem light
+  img.setAttribute('src', './assets/avatar-light.png')
+  } else { 
+  // se tiver sem light mode, manter a imagem normal
+  img.setAttribute('src', './assets/avatar.png')
   }
 }
